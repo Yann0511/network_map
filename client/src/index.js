bc3c35484633ee33c9051fe9f6b81f5a7c5444de
Yann Kelly Olou <yolou@celeriteholding.com>
2022-08-14 18:39:02 +0100

    test

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,7 @@
 var m = require("mithril");
-const login = require("./views/login");
-var register = require("./views/register");
 
 const {
     mountRoutes
 } = require("./mounter");
 
-if (window.localStorage['jwt'] != undefined)
-    mountRoutes();
-else
-     m.route(document.body, "/login", {
-            "/login": {
-                render: function() {
-                    return m(login);
-                }
-            },
-            "/register": {
-                render: function() {
-                    return m(register);
-                }
-            },
-        });
+mountRoutes();
--- client/src/mounter.js
+++ client/src/mounter.js
@@ -1,31 +1,15 @@
 var m = require("mithril");
-var layout = require("./views/layout");
-const sidebar_dashboard = require("./components/sidebar/sidebar_dashboard");
-const dashboard = require("./views/dashboard");
-const sidebar_client_mng = require("./components/sidebar/sidebar_client_mng");
-const client_mng = require("./views/client_mng");
-const sidebar_configuration = require("./components/sidebar/sidebar_configuration");
-const configuration = require("./views/configuration");
+const table = require("./views/table");
 
 
 function mountRoutes() {
     document.body.className = "";
-        m.route(document.body, "/dashboard", {
-            "/dashboard": {
+        m.route(document.body, "/tables", {
+            "/tables": {
                 render: function() {
-                    return m(layout, m(sidebar_dashboard), m(dashboard));
+                    return m(table);
                 }
             },
-            "/gestion_client": {
-                render: function(){
-                    return m(layout, m(sidebar_client_mng), m(client_mng))
-                }
-            },
-            "/configuration": {
-                render: function(){
-                    return m(layout, m(sidebar_configuration), m(configuration))
-                }
-            }
         });
 }
 exports.mountRoutes = mountRoutes;
